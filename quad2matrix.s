5b7c3b290e18007eff4dddd5f0b7dc88d31846bc
evecruz <evecruz@galileo.edu>
2016-04-02 16:59:40 -0700

    first commit

---
+++ quad2matrix.s
@@ -0,0 +1,26 @@
+.text
+
+# Decodes a quadtree to the original matrix
+#
+# Arguments:
+#     quadtree (qNode*)
+#     matrix (void*)
+#     matrix_width (int)
+#
+# Recall that quadtree representation uses the following format:
+#     struct qNode {
+#         int leaf;
+#         int size;
+#         int x;
+#         int y;
+#         int gray_value;
+#         qNode *child_NW, *child_NE, *child_SE, *child_SW;
+#     }
+
+quad2matrix:
+
+        # YOUR CODE HERE #
+    
+    # $a0 : quadtree
+    # $a1 : matrix
+    # $a2: width
