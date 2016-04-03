b58d96192f692474a5681d0e92c3660947aaa794
evecruz <evecruz@galileo.edu>
2016-04-02 17:14:49 -0700

    random

--- lfsr_random.s
+++ lfsr_random.s
@@ -14,9 +14,27 @@ lfsr_random:
 
         la $t0 lfsr
         lhu $v0 0($t0)
+        li $t1 0 # i = 0
 
-        # YOUR CODE HERE #
+        for: 
+        	bgt $t1 15 finish # if i> 15 salta al final
+        	srl $t2 $v0 0 #reg >> 0
+        	srl $t3 $v0 2 #reg >> 2
+        	srl $t4 $v0 3 #reg >> 3
+        	srl $t5 $v0 5 #reg >> 5
+        	xor $t2 $t2 $t3 #(reg >> 0) ^ (reg >> 2)
+        	xor $t3 $t2 $t4 #(reg >> 0) ^ (reg >> 2) ^ (reg >> 3)
+        	xor $t4 $t3 $t5 #highest=(reg >> 0) ^ (reg >> 2) ^ (reg >> 3) ^ (reg >> 5)
+        	srl $t6 $v0 1 #reg >> 1
+        	sll $t7 $t4 15 #highest << 15
+        	or $t8 $t6 $t7 #reg = (reg >> 1) | (highest << 15)
+        	andi $t8, $t8, 0xFFFF #mascara para que sean 16 bits
+        	addi $t1 $t1 1 # i++
+        	addi $v0 $t8 0 #guardando la respuesta en la variable $v0 para desplegar
+        	j for #repite el for hasta que deje de cumplirse la condicion
 
+        finish:
+        li $t1 0 # i = 0
         la $t0 lfsr
         sh $v0 0($t0)
         jr $ra
