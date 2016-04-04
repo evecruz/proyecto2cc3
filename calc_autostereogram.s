5f881c06de2f99f840dc78bedc9842cffd97f551
evecruz <evecruz@galileo.edu>
2016-04-03 19:32:17 -0700

    actualizacion random

--- calc_autostereogram.s
+++ calc_autostereogram.s
@@ -18,9 +18,9 @@ calc_autostereogram:
         sw $s2 8($sp)
         sw $s3 12($sp)
         sw $s4 16($sp)
-	sw $s5 20($sp)
-	sw $s6 24($sp)
-	sw $ra 28($sp)
+        sw $s5 20($sp)#Contador i
+        sw $s6 24($sp)#Contador j
+        sw $ra 28($sp)#Posicion del memoria para etiquetas
         # autostereogram
         lw $s0 32($sp)
         # depth_map
@@ -33,28 +33,48 @@ calc_autostereogram:
         lw $s4 48($sp)
 
         #Declaramos contadores auxiliares i,j
-	addiu $s5,$zero,0 #esto es i
-	addiu $s6,$zero,0 #esto es j
-	
-	for1:
-		addiu $s5,$zero,0 #reinicio contador de ancho		
-	for2:
-		beq $s5,$s2,for1
-		beq $s5,$s4,false
-		#aqui tiene que ir lo del random
-	false:
-		 
-		mulu $t1, $s6,$s2
-		addu $t1,$t1,$s5
-		addu $t1,$t1,$s1 #index del depthMap
-		lb $t3, 0($t1) #valor del puntero en depth(i,j)
-		sub $t3,$t3,$s4 #resto i con S
-		addu $t3,$t3,$s5 # i + depth(i,j)-S
-	salida:  
+        addiu $s5,$zero,0 #esto es i
+        addiu $s6,$zero,0 #esto es j
+        
+        
+        for2:
+                beq $s5,$s2,for1 #i=width
+                beq $s6,$s3,imas
+                bge $s5,$s4,false #i>=S
+                jal lfsr_random  #llamar funcion random
+                move $t6, $v0   #guardamos el valor de random en $t6
+                                       
+        false:
+                 
+                mulu $t1, $s6,$s2 #j*ancho
+                addu $t1,$t1,$s5 #le sumamos posicion i
+                addu $t1,$t1,$s1 #index del depthMap
+                mulu $t2,$s6,$s2#j*ancho
+                addu $t2,$s5,$t2
+                addu $t2,$t2,$s0
+                lb $t3, 0($t1) #valor del puntero en depth(i,j)
+                sub $t3,$t3,$s4 #resto i con S
+                addu $t3,$t3,$s5 # i + depth(t4i,j)-S
+                and $t4, $t4, 0xFF #mascara solicitada en las especificaciones
+                sb $t6 0($t4)#ingresamos el indice I
+                addi $s6,$s6,1 #j++
+                j for2
+        for1:
+                beq $s5,$s2,salida#sale cuando i=ancho
+                addi $s5,$s5,1 #i++
+                j for2
+                
+        imas:
+                addi $s6, $s6, 1 #i++
+        salida:  
         lw $s0 0($sp)
         lw $s1 4($sp)
         lw $s2 8($sp)
         lw $s3 12($sp)
         lw $s4 16($sp)
-        addiu $sp $sp 20
+        lw $s5 20($sp)
+        lw $s6 24($sp)
+        lw $ra 28($sp)
+        #regresamos espacio de memoria
+        addiu $sp $sp 28
         jr $ra
